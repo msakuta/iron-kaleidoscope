5b64bac850987453d4449f55baf1896260451aa6
msakuta <masahiro.sakuta@gmail.com>
2021-01-24 14:50:35 +0900

    WIP on modernizing chapter 2

--- chapters/2/src/main.rs
+++ chapters/2/src/main.rs
@@ -1,36 +1,24 @@
-extern crate rustc_serialize;
-extern crate docopt;
+use crate::driver::{main_loop, IR, Tokens, AST};
+use structopt::StructOpt;
 
-extern crate iron_kaleidoscope;
-
-use iron_kaleidoscope::driver::{main_loop,
-                                IR,
-                                AST,
-                                Tokens
-};
-
-use docopt::Docopt;
-
-const USAGE: &'static str = "
-Usage: iron_kaleidoscope [(-l | -p | -i)]
-
-Options:
-    -l  Run only lexer and show its output.
-    -p  Run only parser and show its output.
-    -i  Run only IR builder and show its output.
-";
-
-#[derive(Debug, RustcDecodable)]
+#[derive(Debug, StructOpt)]
+#[structopt(name = "Args")]
 struct Args {
+    /// Run only lexer and show its output
+    #[structopt(short = "l", long)]
     flag_l: bool,
+
+    /// Run only parser and show its output.
+    #[structopt(short = "p", long)]
     flag_p: bool,
-    flag_i: bool
+
+    /// Run only IR builder and show its output.
+    #[structopt(short = "i", long)]
+    flag_i: bool,
 }
 
 fn main() {
-    let args: Args = Docopt::new(USAGE)
-        .and_then(|d| d.decode())
-        .unwrap_or_else(|e| e.exit());
+    let args = Args::from_args();
 
     let stage = if args.flag_l {
         Tokens
